5bb8cb70299d41929f55a6d68fe6cb79b8cf341b
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-04-25 05:46:30 +0900

    add array type

--- 02_project/app.js
+++ 02_project/app.js
@@ -10,5 +10,12 @@
 var person = {
     name: "ono5",
     age: 34,
+    hobbies: ["Sports", "Cooking"],
 };
+var favoriteActivities;
+favoriteActivities = ["Sports"];
 console.log(person.name);
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
--- 02_project/app.ts
+++ 02_project/app.ts
@@ -11,6 +11,17 @@
 const person = {
   name: "ono5",
   age: 34,
+  hobbies: ["Sports", "Cooking"],
+  role: [2, "author"],
 };
 
+person.role.push("admin");
+
+let favoriteActivities: string[];
+favoriteActivities = ["Sports"];
+
 console.log(person.name);
+
+for (const hobby of person.hobbies) {
+  console.log(hobby.toUpperCase());
+}
