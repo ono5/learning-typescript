8787ad481e6fd53698065b8e24dde22d6a553d89
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-04-25 05:53:56 +0900

    add enum type

--- 02_project/app.js
+++ 02_project/app.js
@@ -1,23 +1,15 @@
-// not best practice
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-//   name: "ono5",
-//   age: 34,
-// };
-// best practice
-var person = {
+var Role;
+(function (Role) {
+    Role[Role["ADMIN"] = 0] = "ADMIN";
+    Role[Role["READ_ONLY"] = 1] = "READ_ONLY";
+    Role[Role["AUTHOR"] = 2] = "AUTHOR";
+})(Role || (Role = {}));
+var person2 = {
     name: "ono5",
     age: 34,
-    hobbies: ["Sports", "Cooking"],
-    role: [2, "author"],
+    hobbies: ["Programming", "Movies"],
+    role: Role.ADMIN,
 };
-person.role.push("admin");
-var favoriteActivities;
-favoriteActivities = ["Sports"];
-console.log(person.name);
-for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
-    var hobby = _a[_i];
-    console.log(hobby.toUpperCase());
+if (person2.role == Role.ADMIN) {
+    console.log("ADMIN USER");
 }
--- 02_project/app.ts
+++ 02_project/app.ts
@@ -1,36 +1,16 @@
-// not best practice
-// const person: {
-//   name: string;
-//   age: number;
-// } = {
-//   name: "ono5",
-//   age: 34,
-// };
+enum Role {
+  ADMIN,
+  READ_ONLY,
+  AUTHOR,
+}
 
-// best practice
-const person: {
-  name: string;
-  age: number;
-  hobbies: string[];
-  role: [number, string];
-} = {
+const person2 = {
   name: "ono5",
   age: 34,
-  hobbies: ["Sports", "Cooking"],
-  role: [2, "author"],
+  hobbies: ["Programming", "Movies"],
+  role: Role.ADMIN,
 };
 
-person.role.push("admin");
-// NG
-// person.role[1] = 10;
-// OK
-// person.role = [0, "admin"];
-
-let favoriteActivities: string[];
-favoriteActivities = ["Sports"];
-
-console.log(person.name);
-
-for (const hobby of person.hobbies) {
-  console.log(hobby.toUpperCase());
+if (person2.role == Role.ADMIN) {
+  console.log("ADMIN USER");
 }
