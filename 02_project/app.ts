c287df70fa5bb081bd3d399770948291df329c8b
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-04-26 07:29:35 +0900

    never and unkown type code

--- 02_project/app.ts
+++ 02_project/app.ts
@@ -1,28 +1,17 @@
-function add(n1: number, n2: number) {
-  return n1 + n2;
+let userInput: unknown;
+let userName: string;
+
+userInput = 5;
+userInput = "Max";
+
+if (typeof userInput === "string") {
+  userName = userInput;
 }
 
-function printResult(num: number): void {
-  console.log("Result: ", num);
+// never type don't return any things!
+function generateError(message: string, code: number): never {
+  throw { message: message, errorCode: code };
 }
 
-function addAndHandle(n1: number, n2: number, cb: (num: number) => void) {
-  const result = n1 + n2;
-  cb(result);
-}
-
-addAndHandle(10, 20, (result) => {
-  console.log(result);
-  return result;
-});
-
-// Function Type
-// let combineValues: Function;
-let combineValues: (a: number, b: number) => number;
-combineValues = add;
-// combineValues = printResult;
-// combineValues = 5;
-
-console.log(combineValues(8, 8));
-
-printResult(add(5, 12));
+const result = generateError("Error happend!", 500);
+console.log(result);
