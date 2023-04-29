797251ac4f55b163d4effdba9fc49b35f48c04ae
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-04-30 06:28:43 +0900

    add class code

--- 04_class_interface/dist/app.js
+++ 04_class_interface/dist/app.js
@@ -1,23 +1,8 @@
 "use strict";
-const userName = "Max";
-let age = 30;
-age = 29;
-const add = (a, b = 1) => a + b;
-const printOutput = (output) => {
-    console.log(output);
-};
-printOutput(add(5));
-const button = document.querySelector("button");
-if (button) {
-    button.addEventListener("click", (event) => {
-        console.log(event);
-    });
+class Department {
+    constructor(n) {
+        this.name = n;
+    }
 }
-const hobbies = ["Sports", "Cooking"];
-const activeHobbies = ["Hiking"];
-activeHobbies.push(...hobbies);
-const person = {
-    name: "Max",
-    age: 30,
-};
-const copiedPerson = Object.assign({}, person);
+const accounting = new Department("Accounting");
+console.log(accounting.name);
