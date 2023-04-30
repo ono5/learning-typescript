04da2bf8b732e60b33d7421521bfa5f3f63b0168
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-05-01 05:56:27 +0900

    add class

--- 04_class_interface/dist/app.js
+++ 04_class_interface/dist/app.js
@@ -1,8 +1,67 @@
 "use strict";
-class Department {
-    constructor(n) {
-        this.name = n;
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+var Department = (function () {
+    function Department(id, name) {
+        this.id = id;
+        this.name = name;
+        this.employees = [];
     }
-}
-const accounting = new Department("Accounting");
-console.log(accounting.name);
+    Department.prototype.describe = function () {
+        console.log("Department {this.id}: ".concat(this.name));
+    };
+    Department.prototype.addEmployee = function (employee) {
+        this.employees.push(employee);
+    };
+    Department.prototype.printEmployeeInformation = function () {
+        console.log(this.employees.length);
+        console.log(this.employees);
+    };
+    return Department;
+}());
+var ITDepartment = (function (_super) {
+    __extends(ITDepartment, _super);
+    function ITDepartment(id, admins) {
+        var _this = _super.call(this, id, "IT") || this;
+        _this.admins = admins;
+        _this.admins = admins;
+        return _this;
+    }
+    return ITDepartment;
+}(Department));
+var AccountingDepartment = (function (_super) {
+    __extends(AccountingDepartment, _super);
+    function AccountingDepartment(id, reports) {
+        var _this = _super.call(this, id, "Accounting") || this;
+        _this.reports = reports;
+        return _this;
+    }
+    AccountingDepartment.prototype.addReport = function (text) {
+        this.reports.push(text);
+    };
+    AccountingDepartment.prototype.printReports = function () {
+        console.log(this.reports);
+    };
+    return AccountingDepartment;
+}(Department));
+var it = new ITDepartment("d1", ["Max"]);
+it.addEmployee("MR.MANY");
+it.addEmployee("MS.MONICA");
+it.describe();
+it.printEmployeeInformation();
+var accounting = new AccountingDepartment("d2", []);
+accounting.addReport("Daily Meetings");
+accounting.printReports();
--- 04_class_interface/src/app.ts
+++ 04_class_interface/src/app.ts
@@ -1,10 +1,58 @@
 class Department {
-  name: string;
+  // name: string;
+  // private readonly id: string
+  private employees: string[] = [];
 
-  constructor(n: string) {
-    this.name = n;
+  constructor(private readonly id: string, public name: string) {
+    // this.name = n;
+  }
+
+  describe(this: Department) {
+    console.log(`Department {this.id}: ${this.name}`);
+  }
+
+  addEmployee(employee: string) {
+    this.employees.push(employee);
+  }
+
+  printEmployeeInformation() {
+    console.log(this.employees.length);
+    console.log(this.employees);
   }
 }
 
-const accounting = new Department("Accounting");
-console.log(accounting.name);
+class ITDepartment extends Department {
+  constructor(id: string, private admins: string[]) {
+    super(id, "IT");
+    this.admins = admins;
+  }
+}
+
+class AccountingDepartment extends Department {
+  constructor(id: string, private reports: string[]) {
+    super(id, "Accounting");
+  }
+
+  addReport(text: string) {
+    this.reports.push(text);
+  }
+
+  printReports() {
+    console.log(this.reports);
+  }
+}
+
+const it = new ITDepartment("d1", ["Max"]);
+it.addEmployee("MR.MANY");
+it.addEmployee("MS.MONICA");
+// it.employees[2] = "Anna";
+
+it.describe();
+it.printEmployeeInformation();
+
+// const accountingCopy = { name: "DUMMY", describe: it.describe };
+// accountingCopy.describe();
+
+const accounting = new AccountingDepartment("d2", []);
+accounting.addReport("Daily Meetings");
+accounting.printReports();
