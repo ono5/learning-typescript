bd14d21eaca77da79e8d0b5d8b568582d11cbabf
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-05-04 05:35:42 +0900

    another tips

--- 04_class_interface/src/app.ts
+++ 04_class_interface/src/app.ts
@@ -1,19 +1,37 @@
 // interfaceはオブジェクトがどんな形であるか定義するためのもの
-interface Greetable {
-  name: string;
+// type AddFn = (a: number, b: number) => number;
+interface AddFn {
+  (a: number, b: number): number;
+}
+let add: AddFn;
+add = (n1: number, n2: number) => {
+  return n1 + n2;
+};
 
+interface Named {
+  readonly name?: string;
+  outputName?: string;
+}
+
+interface Greetable extends Named {
   greet(phrase: string): void;
 }
 
 class Person implements Greetable {
-  name: string;
+  name?: string;
   age = 30;
 
-  constructor(n: string) {
+  constructor(n?: string) {
+    if (n) {
+      this.name = n;
+    }
     this.name = n;
   }
 
   greet(phrase: string) {
+    if (this.name) {
+      console.log(phrase);
+    }
     console.log(phrase + " " + this.name);
   }
 }
