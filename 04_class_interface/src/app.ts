ef0088769d92483ee4ad0f04018d7233f719f733
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-05-08 05:16:52 +0900

    add generic functions

--- 04_class_interface/src/app.ts
+++ 04_class_interface/src/app.ts
@@ -23,3 +23,28 @@ const mergeObj2 = merge({ name: "Max", hobbies: ["Sports"] }, { age: 30 });
 //   { age: 30 }
 // );
 console.log(mergeObj2);
+
+interface Lengthy {
+  length: number;
+}
+
+function countAndDescribe<T extends Lengthy>(element: T): [T, string] {
+  let descriptionText = "There is no value.";
+  if (element.length > 0) {
+    descriptionText = "The number of value is " + element.length + ".";
+  }
+  return [element, descriptionText];
+}
+
+console.log(countAndDescribe("Thank you for your hard work"));
+console.log(countAndDescribe(["Sports", "Cooking"]));
+// console.log(countAndDescribe(10)); Error occurs!
+
+function ExtractAndConvert<T extends object, U extends keyof T>(
+  obj: T,
+  key: U
+) {
+  return "Value: " + obj[key];
+}
+
+ExtractAndConvert({ name: "Max" }, "name");
