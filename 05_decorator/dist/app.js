4caf7c645349f02eed2ebdb87ef1795a766dd4f2
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-05-09 22:58:44 +0900

    useful decorator

--- 05_decorator/dist/app.js
+++ 05_decorator/dist/app.js
@@ -5,9 +5,21 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
-function Logger(constructor) {
-    console.log("Outputting Log...");
-    console.log(constructor);
+function Logger(logString) {
+    return function (constructor) {
+        console.log(logString);
+        console.log(constructor);
+    };
+}
+function withTemplate(template, hookId) {
+    return function (constructor) {
+        const hookEl = document.getElementById(hookId);
+        const p = new constructor();
+        if (hookEl) {
+            hookEl.innerHTML = template;
+            hookEl.querySelector("h1").textContent = p.name;
+        }
+    };
 }
 let Person = class Person {
     constructor() {
@@ -16,7 +28,7 @@ let Person = class Person {
     }
 };
 Person = __decorate([
-    Logger
+    withTemplate("<h1>Person Object</h1>", "app")
 ], Person);
 const pers = new Person();
 console.log(pers);
--- 05_decorator/src/app.ts
+++ 05_decorator/src/app.ts
@@ -1,9 +1,23 @@
-function Logger(constructor: Function) {
-  console.log("Outputting Log...");
-  console.log(constructor);
+function Logger(logString: string) {
+  return function (constructor: Function) {
+    console.log(logString);
+    console.log(constructor);
+  };
 }
 
-@Logger
+function withTemplate(template: string, hookId: string) {
+  return function (constructor: any) {
+    const hookEl = document.getElementById(hookId);
+    const p = new constructor();
+    if (hookEl) {
+      hookEl.innerHTML = template;
+      hookEl.querySelector("h1")!.textContent = p.name;
+    }
+  };
+}
+
+// @Logger("Outputting Log - Person")
+@withTemplate("<h1>Person Object</h1>", "app")
 class Person {
   name = "Max";
 
