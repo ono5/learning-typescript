2d868101944e008a9a575a3c66521fdcc5f77137
ono5 <kuroneko_head_lord@yahoo.co.jp>
2023-05-09 23:16:19 +0900

    add accessor and decorator

--- 05_decorator/src/app.ts
+++ 05_decorator/src/app.ts
@@ -38,6 +38,32 @@ console.log(pers);
 function Log(target: any, propertyName: string | Symbol) {
   console.log("Property Decorator");
   console.log(target.propertyName);
+  console.log(propertyName);
+}
+
+function Log2(target: any, name: string, descriptor: PropertyDescriptor) {
+  console.log("Accessor Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log3(
+  target: any,
+  name: string | Symbol,
+  descriptor: PropertyDescriptor
+) {
+  console.log("Method Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(descriptor);
+}
+
+function Log4(target: any, name: string | Symbol, position: number) {
+  console.log("Parameter Decorator");
+  console.log(target);
+  console.log(name);
+  console.log(position);
 }
 
 class Product {
@@ -45,6 +71,7 @@ class Product {
   title: string;
   private _price: number;
 
+  @Log2
   set price(val: number) {
     if (val > 0) {
       this._price = val;
@@ -58,7 +85,8 @@ class Product {
     this._price = p;
   }
 
-  getPriceWithTax(tax: number) {
+  @Log3
+  getPriceWithTax(@Log4 tax: number) {
     return this._price + (1 + tax);
   }
 }
